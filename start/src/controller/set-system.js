bbf27877fc0dc8f77e2378b82dd4f3d5ab2987f9
xiaoguo0426 <740644717@qq.com>
2020-01-18 00:43:11 +0800

    update

--- start/src/controller/set-system.js
+++ start/src/controller/set-system.js
@@ -32,18 +32,26 @@ layui.define(['table', 'form'], function (exports) {
         //         });
         //     }
         // });
-        hyperf.http.auto(obj);
+        hyperf.http.auto(obj, {
+            done: function (res) {
+                hyperf.msg.success(res.msg);
+
+                let d = obj.field;
+
+                document.getElementsByTagName('title')[0].innerText = d.site;
+
+                $('#site-title').text(d.site);
+
+                $("meta[name='keywords']").attr('content', d.keywords);
+                $("meta[name='description']").attr('content', d.desc);
+                $("meta[name='author']").attr('content', d.author);
+                $("meta[name='copyright']").attr('content', d.copyright);
+            }
+        });
     });
 
     form.on('submit(lay-smtp-info)', function (obj) {
         //提交修改
-        // hyperf.http.auto(obj, {
-        //     done: function (res) {
-        //         hyperf.msg.success(res.msg, function () {
-        //             hyperf.page.refresh();
-        //         });
-        //     }
-        // });
         hyperf.http.auto(obj);
     });
 
